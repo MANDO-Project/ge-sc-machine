1941d06efda9dbc0a25d037ddee4073dde1c91c8
DoanHongPhuc <doanhongphuc0111@gmail.com>
2022-06-09 14:41:45 +0700

    commit code

--- node_modules/aframe-extras/src/loaders/index.js
+++
@@ -1,5 +0,0 @@
-require('./animation-mixer');
-require('./collada-model-legacy');
-require('./fbx-model');
-require('./gltf-model-legacy');
-require('./object-model');
--- node_modules/aframe/src/shaders/index.js
+++
@@ -1,7 +0,0 @@
-require('./flat');
-require('./standard');
-require('./phong');
-require('./sdf');
-require('./msdf');
-require('./ios10hls');
-require('./shadow');
